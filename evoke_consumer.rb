eacf0e123165536bccc50f3b2cdae73e84e6a820
Justin Knowlden <gus@gusg.us>
2009-07-27 16:58:37 -0500

    Updated logo icon

--- evoke_consumer.rb
+++ evoke_consumer.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-ENV['APP_ENV'] ||= 'production'
+ENV['APP_ENV'] ||= ENV['RACK_ENV'] || 'production'
 require File.join(File.dirname(__FILE__), 'config', 'boot')
 
 Delayed::Worker.logger = Logger.new("log/#{ENV['APP_ENV']}-consumer.log")
